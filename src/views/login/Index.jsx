4e06e18a9d0aa8ec4ccc2f82be3f1a7e13e8f675
AriesGenius <zjs1115170255@gmail.com>
2023-08-29 23:11:07 +0800

    login page

---
+++ src/views/login/Index.jsx
@@ -0,0 +1,84 @@
+import { Button, Col, Row } from "antd";
+import React from "react";
+import style from "./login.module.less";
+import logo from "./imgs/logo.png";
+import { Form } from "antd";
+import { Input } from "antd";
+import { Select } from "antd";
+
+export default function Index() {
+  const onFinish = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <div className={style.wrap}>
+      <Row>
+        <Col span={17} className={style.left}></Col>
+        <Col span={7} className={style.right}>
+          <img src={logo} alt="" />
+          <br />
+          <br />
+          <br />
+          <br />
+          <Form
+            style={{ width: "90%" }}
+            labelAlign="left"
+            labelCol={{ span: 8 }}
+            onFinish={onFinish}
+          >
+            <Form.Item
+              label="Type"
+              name="type"
+              rules={[{ required: true, message: "Please input your email!" }]}
+              initialValue={"1"}
+            >
+              <Select>
+                <Select.Option value="1">Student</Select.Option>
+                <Select.Option value="2">Teacher</Select.Option>
+              </Select>
+            </Form.Item>
+
+            <Form.Item
+              label="Email"
+              name="email"
+              rules={[{ required: true, message: "Please input your email!" }]}
+            >
+              <Input type="text" placeholder="email" />
+            </Form.Item>
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your Password!",
+                },
+              ]}
+            >
+              <Input type="text" placeholder="Password" />
+            </Form.Item>
+
+            <br />
+            <br />
+
+            <Form.Item>
+              <Button
+                block
+                htmlType="submit"
+                type="primary"
+                className={style.btn}
+              >
+                Login
+              </Button>
+            </Form.Item>
+
+            <p style={{ textAlign: "right" }}>
+              <a href="/register">Register</a>
+            </p>
+          </Form>
+        </Col>
+      </Row>
+    </div>
+  );
+}
