ee600b2cf107d5631ab8b1d8249653edb5ca923b
AriesGenius <zjs1115170255@gmail.com>
2023-08-30 16:55:35 +0800

    layout page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,51 @@
-import { useState } from "react";
+import * as React from "react";
+import {
+  Routes,
+  Route,
+  Link,
+  useNavigate,
+  useLocation,
+  Navigate,
+  Outlet,
+} from "react-router-dom";
 
 import Login from "./views/login";
 
-function App() {
+export default function App() {
   return (
     <>
-      <Login />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/work" element={<Layout />}>
+          <Route index element={<PublicPage />} />
+          <Route path="about" element={<About />} />
+        </Route>
+      </Routes>
     </>
   );
 }
 
-export default App;
+function Layout() {
+  return (
+    <>
+      <ul>
+        <li>
+          <Link to="/">Public Page</Link>
+        </li>
+        <li>
+          <Link to="/protected">Protected Page</Link>
+        </li>
+      </ul>
+      right
+      <Outlet />
+    </>
+  );
+}
+
+function PublicPage() {
+  return <h3>Public</h3>;
+}
+
+function About() {
+  return <h3>About</h3>;
+}
