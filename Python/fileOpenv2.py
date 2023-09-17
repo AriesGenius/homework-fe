01cc4aa6ec48e137bf99acbd1b9ce4002e4a5811
mcclt001 <73449739+mcclt001@users.noreply.github.com>
2023-09-17 22:56:38 +0930

    Add files via upload

--- Python/fileOpenv2.py
+++ Python/fileOpenv2.py
@@ -1,14 +1,20 @@
 # file must first be converted to a text file.
 file = input("Please input the location of the file to open: ")
 
+# read the file
 fileOpen = open(file, "r")
 
+# the string which represents the types of relationships
 relationshipArrow = "lt"
+# a list for counting how many times the relationshipArrow string is seen
 arrowCount = []
 
+# the string which represents the class names in the file
 className = "<panel_attributes>_"
+# a list for all the class names
 classes = []
 
+# the strings representing where the coordinates of classes and arrows are in the file
 xCoordinateStart = "<x>"
 yCoordinateStart = "<y>"
 wCoordinateStart = "<w>"
@@ -18,41 +24,46 @@ yCoordinateEnd = "</y>"
 wCoordinateEnd = "</w>"
 hCoordinateEnd = "</h>"
 
+# listing the coordinates
 xCoordinate = []
 yCoordinate = []
 wCoordinate = []
 hCoordinate = []
 
 for line in fileOpen:
+    # counting the number of times the reationshipArrow string is seen per line in the file
     count = 0
-    letterCount = 0
+    
     if relationshipArrow in line:
         for letter in line:
+            # '&' represents the arrows
             if letter == "&":
                 count = count + 1
+            # '.' represents a dotted line
             if letter == ".":
                 count = count + 0.1
         arrowCount.append(count)
 
+    # appending the coordinates if they are found in the line of the file.
     elif xCoordinateStart in line:
         xCoordinate.append(line)
-        print(xCoordinate)
     elif yCoordinateStart in line:
         yCoordinate.append(line)
-        print(yCoordinate)
     elif wCoordinateStart in line:
         wCoordinate.append(line)
-        print(wCoordinate)
     elif hCoordinateStart in line:
         hCoordinate.append(line)
-        print(hCoordinate)
 
+    # appending the class name it is found in the line of the file
     elif className in line:
         classes.append(line)
 
 print(arrowCount)
 
+# listing the names of the relationships
 relationships = []
+
+# The different numbers represent what type of relationship
 for index in arrowCount:
     if index == 2:
         relationships.append("Inheritance")
@@ -69,11 +80,12 @@ for index in arrowCount:
     elif index == 5:
         relationships.append("Composition")
 
-print(relationships)
+# printing everything
+print("The relationships are", relationships)
 
-print("The x coordinate is:", xCoordinate)
-print("The y coordinate is:", yCoordinate)
-print("The w coordinate is:", wCoordinate)
-print("The h coordinate is:", hCoordinate)
+print("The x coordinates are:", xCoordinate)
+print("The y coordinates are:", yCoordinate)
+print("The w coordinates are:", wCoordinate)
+print("The h coordinates are:", hCoordinate)
 
-print(classes)
+print("The classes are:", classes)
